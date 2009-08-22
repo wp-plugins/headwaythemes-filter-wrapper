0507bd11c3a31e63d41a253158401da93cf1affa
jeannot.muller <jeannot.muller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-22 10:37:01 +0000

    1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/headwaythemes-filter-wrapper/tags/1.0.2@148079 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ headwaythemes-filter-wrapper.php
@@ -0,0 +1,98 @@
+<?php
+/*
+Plugin Name: headwaythemes-filter-wrapper
+Plugin URI: http://www.ramgad.com/hfw/
+Description: Possibility for non-programmers to use some filters through the setting's panel of this plugini. Works only with the headwaythemes.com!
+Version: 1.0.2
+Author: Jeannot Muller
+Author URI: http://www.ramgad.com/
+Min WP Version: 2.1
+Max WP Version: 2.9
+*/
+
+// Update routines
+	if ('insert' == $HTTP_POST_VARS['action_hfw']) {
+    	update_option("hfw_link",$HTTP_POST_VARS['hfw_link']);
+    	update_option("hfw_copyright",$HTTP_POST_VARS['hfw_copyright']);
+    	update_option("hfw_go_to_top",$HTTP_POST_VARS['hfw_go_to_top']);
+}
+
+if (!class_exists('hfw_main')) {
+    class hfw_main {
+		/**
+		* PHP 4 Compatible Constructor
+		*/
+		function hfw_main(){$this->__construct();}
+		
+		/**
+		* PHP 5 Constructor
+		*/		
+		function __construct(){
+			// Registrieren der WordPress-Hooks
+			add_action('admin_menu', 'hfw_description_add_menu');
+			add_filter('headway_link', 'hfw_link', 1);
+			add_filter('headway_copyright', 'hfw_copyright', 1);
+			add_filter('headway_go_to_top', 'hfw_go_to_top', 1);
+		}
+		// Registration of WordPress-Hooks
+	}
+
+function hfw_description_option_page() {
+	?>
+
+	<!-- Start Options Adminarea (xhtml) -->
+	  <div class="wrap">
+	    <h2>Headwaythemes-Filter-Wrapper Options</h2>
+		         <div style="margin-top:20px;">
+		         <form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>&amp;updated=true">
+		                          <div style="">
+						<table class="form-table">
+						<tr><th scope="col" colspan="3" cellpadding="15">Settings</th></tr>
+		                                <tr><th scope="row">headway_link:</th><td>
+		                    <input name="hfw_link" size="60" value="<?=get_option("hfw_link");?>" type="text" /></td></tr>
+						<tr><th scope="row">headway_copyright:</th><td>
+		                    <input name="hfw_copyright" size="60" value="<?=get_option("hfw_copyright");?>" type="text" /></td></tr>
+						<tr><th scope="row">headway_go_to_top</th><td>
+			            <input name="hfw_go_to_top" size="60" value="<?=get_option("hfw_go_to_top");?>" type="text" /></td></tr>
+</table>
+	      <br>
+	      <p class="submit_hfw"><input name="submit_hfw" type="submit" id="submit" value="Save changes &raquo;">
+	     <input class="submit_hfw" name="action_hfw" value="insert": type="hidden" /></p>
+	    </form>
+	  </div>
+	</div>
+	<p style="text-align:justify;">After saving these changes will be active immediately on your <a href="http://headwaythemes.com/" target="_blank">Headwaythemes</a>. Please be aware that your need a DEVELOPER LICENSE to overwrite the original link back to headway themes! Respect Copyright!
+	<p style="text-align:justify;">If you have problems with HFW (headways-filter-wrapper), please feel free to contact me: <a href="mailto:jeannot.muller@ramgad.com">jeannot.muller@ramgad.com</a></p>
+
+<?php
+
+} // Ende Funktion hfw_description_option_page()
+
+// Adminmenu Optionen erweitern
+function hfw_description_add_menu() {
+      global $hfw_link, $hfw_copyright, $hfw_go_to_top;
+      add_options_page('HFW', 'HFW', 9, __FILE__, 'hfw_description_option_page'); //optionenseite hinzufügen
+}
+
+function hfw_link() {
+   $hfw_link = get_option('hfw_link');
+   return $hfw_link;
+}
+
+function hfw_copyright() {
+	$hfw_copyright = get_option('hfw_copyright');
+	return $hfw_copyright;
+}
+
+function hfw_go_to_top() {
+	$hfw_go_to_top = get_option('hfw_go_to_top');
+	return $hfw_go_to_top;
+}
+}
+
+
+//instantiate the class
+if (class_exists('hfw_main')) {
+	$hfw_main = new hfw_main();
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== headwaythemes-filter-wrapper ===
+Contributors: jeannot.muller
+Donate link: http://www.ramgad.com/
+Tags: headway,headway themes,headwaythemes,wrapper,filters,hooks
+Requires at least: 2.5.0
+Tested up to: 2.9
+Stable tag: 1.0.2
+
+Possibility for non-programmers to use some filters through the setting's panel of this plugin. 
+
+== Description ==
+Possibility for non-programmers to use some filters through the setting's panel of this plugin. Works only with the headwaythemes.com!
+
+== Installation ==
+1. Upload 'headwaythemes-filter-wrapper.php' to the '/wp-content/plugins/headwaythemes-filter-wrapper' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Customize the settings under /Settings/HFW/
+4. Have fun, and drop me a comment on <a href="http://www.ramgad.com" target="_blank">ramgad.com</a> if you have any comments, remarks, advices, wishes or if you're just happy.
+
+== Frequently Asked Questions ==
+Please read the FAQ <a href="http://www.ramgad.com/hfw" target="_blank">here</a>.
+
+== Screenshots ==
+1. Option panel
+
+== Arbitrary section ==
+Written by Jeannot Muller, please feel free to contact me: <a href="mailto:jeannot.muller@ramgad.com">jeannot.muller@ramgad.com</a>
